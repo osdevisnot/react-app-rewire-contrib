9098a89173279686ab612ead6075e96484fd198d
osdevisnot <osdevisnot@gmail.com>
2017-07-24 00:57:07 -0700

    handle empty plugins use case

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function rewireEmotion(config, env, emotionBabelOptions = {}) {
   const babelLoader = config.module.rules.find(rule => rule.loader && rule.loader.indexOf('babel-loader') > 0);
-  if (babelLoader && babelLoader.options && babelLoader.options.plugins) {
+  if (babelLoader && babelLoader.options) {
     babelLoader.options.plugins = (babelLoader.options.plugins || []).concat([['emotion/babel', emotionBabelOptions]]);
-  } else if (babelLoader && babelLoader.query && babelLoader.query.plugins) {
+  } else if (babelLoader && babelLoader.query) {
     babelLoader.query.plugins = (babelLoader.query.plugins || []).concat([['emotion/babel', emotionBabelOptions]]);
   }
   return config;
