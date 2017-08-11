f06aadf3375bbb881af5a39b472e4380ebbb4bd2
Andrey Melikhov <amel.truelife@gmail.com>
2017-08-11 18:26:12 +0300

    This commit fixes #3 (#4)

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function rewireEmotion(config, env, emotionBabelOptions = {}) {
-  const babelLoader = config.module.rules.find(rule => rule.loader && rule.loader.indexOf('babel-loader') > 0);
+
+  let babelLoader ;
+
+  const checkRule = rule => rule.loader && rule.loader.indexOf('babel-loader') > 0;
+  config.module.rules.every(rule => {
+    if (rule.oneOf) {
+      babelLoader = (rule.oneOf.find(checkRule))
+    } else if (checkRule(rule)) {
+      babelLoader = rule.loader;
+    }
+    return !babelLoader;
+  });
+
   if (babelLoader && babelLoader.options) {
     babelLoader.options.plugins = (babelLoader.options.plugins || []).concat([['emotion/babel', emotionBabelOptions]]);
   } else if (babelLoader && babelLoader.query) {
