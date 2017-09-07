5be9cf5534dfcde7cb1ce00c05ddf19b0d08f501
Mouad Debbar <mouad.debbar@gmail.com>
2017-09-07 02:05:27 -0700

    Use rewired's injectBabelPlugin

--- packages/react-app-rewire-emotion/index.js
+++ packages/react-app-rewire-emotion/index.js
@@ -1,22 +1,7 @@
-function rewireEmotion(config, env, emotionBabelOptions = {}) {
-  let babelLoader;
+const { injectBabelPlugin } = require('react-app-rewired');
 
-  const checkRule = rule => rule.loader && rule.loader.indexOf('babel-loader') > 0;
-
-  config.module.rules.every(rule => {
-    if (rule.oneOf) {
-      babelLoader = rule.oneOf.find(checkRule);
-    } else if (checkRule(rule)) {
-      babelLoader = rule;
-    }
-    return !babelLoader;
-  });
-
-  if (babelLoader && babelLoader.options) {
-    babelLoader.options.plugins = (babelLoader.options.plugins || []).concat([['emotion/babel', emotionBabelOptions]]);
-  } else if (babelLoader && babelLoader.query) {
-    babelLoader.query.plugins = (babelLoader.query.plugins || []).concat([['emotion/babel', emotionBabelOptions]]);
-  }
+function rewireEmotion(config, env) {
+  config = injectBabelPlugin('emotion/babel', config);
   return config;
 }
 
