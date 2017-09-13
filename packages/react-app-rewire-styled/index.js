150cc1b44648364b127519f424f9dc34f28a0a04
Mouad Debbar <mdebbar@linkedin.com>
2017-09-13 13:36:58 -0700

    peer dep react-app-rewired@^1.2.7

--- packages/react-app-rewire-styled/index.js
+++ packages/react-app-rewire-styled/index.js
@@ -1,24 +1,7 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+
 function rewireStyled(config, env, styledComponentsBabelOptions = {}) {
-  let babelLoader;
-
-  const checkRule = rule => rule.loader && rule.loader.indexOf('babel-loader') > 0;
-
-  config.module.rules.every(rule => {
-    if (rule.oneOf) {
-      babelLoader = rule.oneOf.find(checkRule);
-    } else if (checkRule(rule)) {
-      babelLoader = rule;
-    }
-    return !babelLoader;
-  });
-
-  if (babelLoader && babelLoader.options) {
-    babelLoader.options.plugins = (babelLoader.options.plugins || [])
-      .concat([['styled-components', styledComponentsBabelOptions]]);
-  } else if (babelLoader && babelLoader.query) {
-    babelLoader.query.plugins = (babelLoader.query.plugins || [])
-      .concat([['styled-components', styledComponentsBabelOptions]]);
-  }
+  config = injectBabelPlugin(['styled-components', styledComponentsBabelOptions], config);
   return config;
 }
 
