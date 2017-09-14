3f74746e661c08ab7583ced0ca0474605661e2e0
Mouad Debbar <mouad.debbar@gmail.com>
2017-09-13 23:29:48 -0700

    Use rewired's injectBabelPlugin (#7)
    * Use rewired's injectBabelPlugin
    
    * Allow passing emotionBabelOptions
    
    * peer dep react-app-rewired@^1.2.7

--- packages/react-app-rewire-emotion/index.js
+++ packages/react-app-rewire-emotion/index.js
@@ -1,22 +1,7 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+
 function rewireEmotion(config, env, emotionBabelOptions = {}) {
-  let babelLoader;
-
-  const checkRule = rule => rule.loader && rule.loader.indexOf('babel-loader') > 0;
-
-  config.module.rules.every(rule => {
-    if (rule.oneOf) {
-      babelLoader = rule.oneOf.find(checkRule);
-    } else if (checkRule(rule)) {
-      babelLoader = rule;
-    }
-    return !babelLoader;
-  });
-
-  if (babelLoader && babelLoader.options) {
-    babelLoader.options.plugins = (babelLoader.options.plugins || []).concat([['emotion/babel', emotionBabelOptions]]);
-  } else if (babelLoader && babelLoader.query) {
-    babelLoader.query.plugins = (babelLoader.query.plugins || []).concat([['emotion/babel', emotionBabelOptions]]);
-  }
+  config = injectBabelPlugin(['emotion/babel', emotionBabelOptions], config);
   return config;
 }
 
--- packages/react-app-rewire-lodash/index.js
+++ packages/react-app-rewire-lodash/index.js
@@ -1,22 +1,7 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+
 function rewireLodash(config, env, lodashBabelOptions = {}) {
-  let babelLoader;
-
-  const checkRule = rule => rule.loader && rule.loader.indexOf('babel-loader') > 0;
-
-  config.module.rules.every(rule => {
-    if (rule.oneOf) {
-      babelLoader = rule.oneOf.find(checkRule);
-    } else if (checkRule(rule)) {
-      babelLoader = rule;
-    }
-    return !babelLoader;
-  });
-
-  if (babelLoader && babelLoader.options) {
-    babelLoader.options.plugins = (babelLoader.options.plugins || []).concat([['lodash', lodashBabelOptions]]);
-  } else if (babelLoader && babelLoader.query) {
-    babelLoader.query.plugins = (babelLoader.query.plugins || []).concat([['lodash', lodashBabelOptions]]);
-  }
+  config = injectBabelPlugin(['lodash', lodashBabelOptions], config);
   return config;
 }
 
